1bee9dd02711e8e8aab6287735b9c70055a111df
cagatay00 <jacktherhycer@gmail.com>
2022-12-25 21:39:50 +0300

    android instrument test

--- app/src/androidTest/java/com/example/unolingo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/unolingo/ExampleInstrumentedTest.kt
@@ -2,11 +2,16 @@ package com.example.unolingo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.unolingo.model.Question
+import com.example.unolingo.utils.Utils
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.toObject
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import java.io.File
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +25,28 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.example.unolingo", appContext.packageName)
+    }
+
+    @Test
+    fun controlImagePathStoreImage() {
+
+        val arrayList = ArrayList<Question>()
+        FirebaseFirestore.getInstance().collection("lessons").addSnapshotListener { value, error ->
+            if (error != null) {
+                return@addSnapshotListener
+            }
+            if (value != null) {
+                arrayList.removeAll { true }
+                for (index in value.iterator().withIndex()) {
+                    val document = value.documents[index.index]
+                    val lesson = document.toObject<Question>()
+                    if (lesson != null) {
+                        arrayList.add(lesson)
+                    }
+                }
+                Utils.storeImages(arrayList, InstrumentationRegistry.getInstrumentation().targetContext)
+                assert(File(arrayList[0].filePaths[0]).exists())
+            }
+        }
     }
 }
